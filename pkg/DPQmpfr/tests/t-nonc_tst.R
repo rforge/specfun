b778932ddae824605106b6688e74407c90237b7f
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-08-16 20:45:36 +0000

    tweaks to make test pass
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/specfun@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/DPQmpfr/tests/t-nonc_tst.R
+++ pkg/DPQmpfr/tests/t-nonc_tst.R
@@ -1,7 +1,7 @@
 require(Rmpfr)
 require(DPQmpfr)
 
-stopifnot(all.equal(dnt.1(mpfr(0, 64),  5,10), ## gave NaN
+stopifnot(all.equal(DPQ:::dnt.1(mpfr(0, 64),  5,10), ## gave NaN
                     3.66083172640611114864e-23, tol=1e-20))
 
 dntM(mpfr(-4:4, 256), 5, 10)##--> heureka!
