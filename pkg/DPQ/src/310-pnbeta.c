dcdea460db05e55e98ef2194182501ebfc0b6de7
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-08-14 15:49:25 +0000

    compiles -pedantic -Wall without warning
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/specfun@73 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/DPQ/src/310-pnbeta.c
+++ pkg/DPQ/src/310-pnbeta.c
@@ -17,36 +17,22 @@
 #include <Rmath.h>
 #include <R.h>
 
-double ncbeta_(double a, double b, double lambda, double x, double errmax,
-	       int *ifault);
+double betanc(double x, double a, double b, double lambda,
+	      double errmax, int itrmax, int *ifault);
 
-double betanc_(double x, double a, double b, double lambda,
-	       double errmax, int itrmax, int *ifault);
-
-/* call this from R : */
-void sub_ncbeta(double *a, double *b, double *lambda, double *x, double *errmax,
-		int *ifault, double *res, int *n_res)
+double ncbeta(double a, double b, double lambda, double x,
+	      int itrmax, /* only passed to betanc() for AS 226 currently,
+			   *  was hardwired = 100 */
+	      double errmax,
+	      int *xj, // output: gives the number of iterations taken
+	      int *ifault)
 {
-    int i;
-    for(i=0; i < *n_res; i++)
-	res[i] = ncbeta_(a[i], b[i], lambda[i], x[i],
-			 *errmax, ifault);
-}
 
-double ncbeta_(double a, double b, double lambda, double x, double errmax,
-	       int *ifault)
-{
-    /* Initialized data */
+/*       ALGORITHM AS 310 APPL. STATIST. (1997), VOL. 46, NO. 1
 
-    static int itrmax = 100; /* only for AS 226 currently */
-
-    static double zero = 0.;
-    static double half = 0.5;
-    static double one =  1.;
-    static double five = 5.;
-
-    /* System generated locals */
-    double ret_val, r__1, r__2;
+       Computes the cumulative distribution function of a
+       non-central beta random variable
+*/
 
     /* Builtin functions */
     double sqrt(double), log(double), exp(double), lgamma(double);
@@ -56,104 +42,77 @@ double ncbeta_(double a, double b, double lambda, double x, double errmax,
       double precision function gammad(x, p, ifault)
 */
 
-    /* Local variables */
-    static double c__;
-    static int i__, j, m;
-    static double q, r__, s, t, s0, s1, t0, t1, fx, gx;
-    static int xj;
-    static double ebd, sum, beta, temp, psum;
-    static int iter1, iter2;
-    static double errbd, ftemp;
-    static int iterhi, iterlo;
+    // Check for admissibility of parameters :
 
-
-/*       ALGORITHM AS 310 APPL. STATIST. (1997), VOL. 46, NO. 1
-
-       Computes the cumulative distribution function of a
-       non-central beta random variable
-
-
-
-       Local variable XJ gives the number of iterations taken */
-
-
-
-
-    ret_val = x;
-
-/*       Check for admissibility of parameters */
-
-    *ifault = 3;
-    if (lambda <= zero || a <= zero || b <= zero) {
-	return ret_val;
+    if (lambda <= 0. || a <= 0. || b <= 0.) {
+	*ifault = 3; return x;
     }
-    *ifault = 2;
-    if (x < zero || x > one) {
-	return ret_val;
+    if (x < 0. || x > 1.) {
+	*ifault = 2; return x;
     }
-    *ifault = 1;
-    if (x == zero || x == one) {
-	return ret_val;
+    if (x == 0. || x == 1.) {
+	*ifault = 1; return x;
     }
     *ifault = 0;
 
-    c__ = lambda * half;
-    xj = zero;
+    *xj = 0;
 
-    if (lambda < 54.f) {
+    if (lambda < 54.) {/* AS 226 as it stands is sufficient in this situation */
 
-/*       AS 226 as it stands is sufficient in this situation */
+	return betanc(x, a, b, lambda, errmax, itrmax, ifault);
 
-	ret_val = betanc_(x, a, b, lambda, errmax, itrmax, ifault);
-	return ret_val;
-    } else {
-	m = (int) (c__ + half);
-	t = five * sqrt((double) m);
+    } else { // lambda >= 54 :
+
+	double q, r__, s, t, s0, s1, t0, t1, fx, gx;
+	double ebd, sum, beta, temp, psum;
+	int iter1, iter2;
+	double errbd, ftemp;
+	int iterhi, iterlo;
+	double c__ = lambda * 0.5;
+
+	int m = (int) (c__ + 0.5);
+	t = 5. * sqrt((double) m);
 	iterlo = m - t;
 	iterhi = m + t;
-	t = -c__ + m * log(c__) - lgamma(m + one);
+	t = -c__ + m * log(c__) - lgamma(m + 1.);
 	q = exp(t);
 	r__ = q;
 	psum = q;
-	r__1 = a + m;
-	r__2 = a + m + b;
+	// r__1 = a + m;
+	// r__2 = a + m + b;
 	beta = lgamma(a + m) + lgamma(b) - lgamma(a + m + b);
 	s1 = (a + m) * log(x) + b * log1p( - x) - log(a + m) - beta;
 	gx = exp(s1);
 	fx = gx;
-	r__1 = a + m;
+	// r__1 = a + m;
 /*	temp = betain_(x, &r__1, b, &beta, ifault); */
 	temp = pbeta(x, a + m, b, /*lower= */TRUE, /*log_p=*/FALSE);
 	ftemp = temp;
-	xj += one;
+	(*xj)++;
 	sum = q - temp;
 	iter1 = m;
 
 /*      The first set of iterations starts from M and goes downwards */
 
 L20:
-	if (iter1 < iterlo) {
-	    goto L30;
-	}
-	if (q < errmax) {
+	if (iter1 < iterlo || q < errmax) {
 	    goto L30;
 	}
 	q -= iter1 / c__;
-	xj += one;
-	gx = (a + iter1) / (x * (a + b + iter1 - one)) * gx;
-	iter1 -= one;
+	(*xj)++;
+	gx = (a + iter1) / (x * (a + b + iter1 - 1.)) * gx;
+	iter1--;
 	temp += gx;
 	psum += q;
 	sum += q * temp;
 	goto L20;
 L30:
-	t0 = lgamma(a + b) - lgamma(a + one) - lgamma(b);
+	t0 = lgamma(a + b) - lgamma(a + 1.) - lgamma(b);
 	s0 = a * log(x) + b * log1p( - x);
-
-	for (i__ = 1; i__ <= iter1; ++i__) {
-	    j = i__ - one;
+	s = 0.;
+	for (double j = 0.; j < iter1; ++j) {
 	    s += exp(t0 + s0 + j * log(x));
-	    t1 = log(a + b + j) - log(a + one + j) + t0;
+	    t1 = log(a + b + j) - log(a + 1. + j) + t0;
 	    t0 = t1;
 	}
 
@@ -166,48 +125,40 @@ L30:
 	gx = fx;
 	iter2 = m;
 L50:
-	ebd = errbd + (one - psum) * temp;
+	ebd = errbd + (1. - psum) * temp;
 	if (ebd < errmax || iter2 >= iterhi) {
 	    goto L60;
 	}
-	iter2 += one;
-	xj += one;
+	iter2 += 1.;
+	(*xj)++;
 	q = q * c__ / iter2;
 	psum += q;
 	temp -= gx;
-	gx = x * (a + b + iter2 - one) / (a + iter2) * gx;
+	gx = x * (a + b + iter2 - 1.) / (a + iter2) * gx;
 	sum += q * temp;
 	goto L50;
 L60:
 	;
+	return sum;
     }
-    ret_val = sum;
 
-    return ret_val;
-} /* ncbeta_ */
+} /* ncbeta */
 
-double betanc_(double x, double a, double b, double lambda,
-	       double errmax, int itrmax, int *ifault)
+/* call this via .C()  from R : */
+void C_ncbeta(double *a, double *b, double *lambda, double *x, double *errmax,
+	      int *itrmax,
+	      int *ifault, double *res, int *n_res)
 {
-    /* Initialized data */
-
-    /* static double errmax = 1e-6; */
-    /* static int itrmax = 100; */
-    static double ualpha = 5.;
-    static double zero = 0.;
-    static double half = 0.5;
-    static double one =  1.;
-
-    /* System generated locals */
-    double ret_val;
-
-    /* Builtin functions */
-    double sqrt(double), log(double), exp(double);
-
-    /* Local variables */
-    static double c__, q, a0, x0, ax, gx, xj, beta, temp, sumq, errbd;
+    int it_used;
+    for(int i=0; i < *n_res; i++)
+	res[i] = ncbeta(a[i], b[i], lambda[i], x[i],
+			*itrmax, *errmax, &it_used, ifault);
+}
 
 
+double betanc(double x, double a, double b, double lambda,
+	      double errmax, int itrmax, int *ifault)
+{
 /*     ALGORITHM AS226 APPL. STATIST. (1987) VOL. 36, NO. 2
      Incorporates modification AS R84 from AS vol. 39, pp311-2, 1990
 
@@ -216,28 +167,34 @@ double betanc_(double x, double a, double b, double lambda,
 
      Auxiliary routines required: LGAMMA - log-gamma function (ACM
      291 or AS 245), and BETAIN - incomplete-beta function (AS 63)
+*/
+    /* Builtin functions */
+    double sqrt(double), log(double), exp(double);
 
+    /* Local variables */
+    static double c__, q, a0, x0, ax, gx, beta, temp, sumq, errbd;
 
-     Change ERRMAX and ITRMAX if desired ... */
+    /* Originally
 
-    ret_val = x;
+    static real errmax = 1e-6f;
+    static integer itrmax = 100;
 
-    *ifault = 2;
-    if (lambda < zero || a <= zero || b <= zero) {
-	return ret_val;
+    * Change ERRMAX and ITRMAX if desired ... */
+
+    if (lambda < 0. || a <= 0. || b <= 0.) {
+	*ifault = 2; return x;
     }
-    *ifault = 3;
-    if (x < zero || x > one) {
-	return ret_val;
+    if (x < 0. || x > 1.) {
+	*ifault = 3; return x;
     }
     *ifault = 0;
-    if (x == zero || x == one) {
-	return ret_val;
+    if (x == 0. || x == 1.) {
+	return x;
     }
 
-    c__ = lambda * half;
+    c__ = lambda * 0.5;
 
-    x0 = floor(fmax2(0., c__ - ualpha * sqrt(c__)));
+    x0 = floor(fmax2(0., c__ - 5. * sqrt(c__)));
     a0 = a + x0;
     beta = lgamma(a + x0) + lgamma(b) - lgamma(a0 + b);
     temp = pbeta(x, a0, b, /*lower= */TRUE, /*log_p=*/FALSE);
@@ -248,32 +205,29 @@ double betanc_(double x, double a, double b, double lambda,
     } else {
 	q = exp(-c__);
     }
-    xj = zero;
+    int j = 0;
     ax = q * temp;
-    sumq = one - q;
-    ret_val = ax;
+    sumq = 1. - q;
+    double ret_val = ax;
 
 /*     Recur over subsequent terms until convergence is achieved... */
-
-L10:
-    xj += one;
-    temp -= gx;
-    gx = x * (a + b + xj - one) * gx / (a + xj);
-    q = q * c__ / xj;
-    sumq -= q;
-    ax = temp * q;
-    ret_val += ax;
+// L10:
+    do {
+	j += 1;
+	temp -= gx;
+	gx = x * (a + b + j - 1.) * gx / (a + j);
+	q = q * c__ / j;
+	sumq -= q;
+	ax = temp * q;
+	ret_val += ax;
 
 /*     Check for convergence and act accordingly... */
+	errbd = (temp - gx) * sumq;
+    } while (j < itrmax && errbd > errmax);
 
-    errbd = (temp - gx) * sumq;
-    if ((int) xj < itrmax && errbd > errmax) {
-	goto L10;
-    }
     if (errbd > errmax) {
 	*ifault = 1;
     }
-
     return ret_val;
-} /* betanc_ */
+} /* betanc */
 
